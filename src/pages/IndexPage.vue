8e73c03d9b51bcfc477db86fdce24134584c6fe5
rei-cher <p4shage@gmail.com>
2024-03-22 14:24:26 -0400

    Basic ToDo

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,159 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-list bordered class="rounded-borders q-mb-md">
+      <q-item 
+        v-for="task in tasks" 
+        :key="task.id" 
+        clickable 
+        v-ripple
+        @click.stop="openTaskDialog(task)">
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+          <q-item-label caption>{{ task.description }}</q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-item-label caption>{{ task.dueTime }}</q-item-label>
+        </q-item-section>
+        <q-item-section side top>
+          <q-btn 
+            flat 
+            round 
+            dense 
+            icon="delete" 
+            @click="removeTask(task.id)" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <!-- Details Dialog-->
+    <q-dialog v-model="taskDialog" full-width full-height>
+      <q-card class="full-height">
+        <q-bar>
+          <q-btn dense flat icon="close" @click="closeTaskDialog"/>
+          <q-space/>
+        </q-bar>
+        <q-card-section class="q-pt-none">
+          <div class="text-h6">{{ selectedTask?.name }}</div>
+          <p>{{ selectedTask?.description }}</p>
+          <div>Due Time: {{ selectedTask?.dueTime }}</div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+    
+    <!-- Task Dialog-->
+    <q-dialog v-model="dialog" full-width full-height>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+        <q-card-section>
+          <div class="text-h6" style="margin-bottom: 20px">Add New Task</div>
+          <q-input 
+            v-model="newTask.name" 
+            :error="nameError" 
+            :error-message="nameErrorMessage" 
+            label="Task Name" 
+            filled 
+            @input="validateName"/>
+          <q-input 
+            v-model="newTask.description" 
+            label="Description" 
+            type="textarea" filled />
+          <q-input 
+            filled 
+            readonly 
+            v-model="newTask.dueTime" 
+            label="Due Time" 
+            hint="Tap to set time" 
+            @click="timeDialog = true" />
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn label="Add" color="primary" @click="addTask" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <!-- Time Picker-->
+    <q-dialog v-model="timeDialog">
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-time v-model="pickedTime" format24h="auto" />
+          <q-btn label="Set Time" color="primary" @click="setTime" />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
+    <div class="absolute-bottom-right">
+      <q-btn fixed fab icon="add" color="green" bottom right @click="openDialog"/>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [],
+      dialog: false,
+      timeDialog: false,
+      pickedTime: '12:00',
+      newTask: {
+        name: '',
+        description: '',
+        dueTime: '',
+      },
+      nameError: false,
+      nameErrorMessage: 'Task name is required',
+      taskDialog: false,
+      selectedTask: null,
+    };
+  },
+  methods: {
+    openDialog() {
+      this.dialog = true;
+    },
+    addTask() {
+      if (this.newTask.name.trim() && this.newTask.dueTime.trim()) {
+        this.tasks.push({
+          ...this.newTask,
+          id: Date.now(),
+        });
+        this.resetNewTask();
+        this.dialog = false;
+      }
+    },
+    removeTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id);
+    },
+    setTime() {
+      this.newTask.dueTime = this.pickedTime;
+      this.timeDialog = false;
+    },
+    resetNewTask() {
+      this.newTask = { name: '', description: '', dueTime: '' };
+      this.pickedTime = '12:00'; // Reset pickedTime for the next use
+    },
+    validateName(){
+      this.nameError = !this.newTask.name.trim();
+      return !this.nameError;
+    },
+    openTaskDialog(task){
+      this.selectedTask = task;
+      this.taskDialog = true;
+    },
+    closeTaskDialog(){
+      this.taskDialog = false;
+      this.selectedTask = null;
+    }
+  },
+};
+</script>
+
+<style>
+  .absolute-bottom-right{
+    position: absolute;
+    right: 16px;
+    bottom: 16px;
+  }
+</style>
